b935504d8c29b239112c77176b9c2e943191a43d
vishal <vk9236730@gmail.com>
2026-01-31 22:20:35 +0530

    modify*

--- hello.c
+++ hello.c
@@ -46,3 +46,23 @@ int main() {
        }
   return 0;
 }
+
+//find a number is prime or not prime
+#include <stdio.h>
+int main(){
+    int n,count=0;
+    printf("Enter any number:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            count++;
+        }
+    }
+    if(count==2){
+            printf("Number is prime");
+    }
+    else{
+            printf("Number is not prime");
+        }
+        return 0;
+ }
