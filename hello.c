11e1f507296828d02df5eea7719d98bd2f44777f
vishal <vk9236730@gmail.com>
2026-02-19 16:14:08 +0530

    Add program

--- hello.c
+++ hello.c
@@ -191,3 +191,17 @@ int main() {
    }
    return 0;
 }
+
+//
+#include <stdio.h>
+int main() {
+   int n,r;
+   printf("Enter Number: ");
+   scanf("%d",&n);
+   while(n>0){
+       r = n%10;
+       printf("%d",r);
+       n = n/10;
+   }
+    return 0;
+}
